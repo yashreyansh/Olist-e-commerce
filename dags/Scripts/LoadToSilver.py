3a6b2c6549c129eebd0ec89c9545be53417acc37
Shreyansh <yashreyansh0@gmail.com>
2026-01-06 01:28:58 -0600

    Updated logic to create separate payment fact and order summary delta table

--- dags/Scripts/LoadToSilver.py
+++ dags/Scripts/LoadToSilver.py
@@ -5,28 +5,13 @@ import glob
 from gamma.Scripts.dependencies.spark import start_spark
 from gamma.Scripts.dependencies.archieveFiles import archiveFile
 from pyspark.sql.types import StructType, StructField, StringType, FloatType, TimestampType, IntegerType, LongType
-from pyspark.sql.functions import col, coalesce,to_timestamp
+from pyspark.sql.functions import col, coalesce,to_timestamp,collect_set, concat_ws,sort_array, max, sum
 from delta.tables import DeltaTable
 from pyspark.sql.utils import AnalysisException
 
 
 def process_available_files():
-    master_schema = StructType(
-        [
-            StructField("order_id", StringType(), False),
-            StructField("customer_id", StringType(), True),
-            StructField("order_status", StringType(), True),
-            StructField("order_approved_at", TimestampType(), True),
-            StructField("order_purchase_timestamp", TimestampType(), True),
-            StructField("order_delivered_carrier_date", TimestampType(), True),
-            StructField("order_delivered_customer_date", TimestampType(), True),
-            StructField("order_estimated_delivery_date", TimestampType(), True),
-            StructField("payment_sequential", LongType(), True),
-            StructField("payment_installments", IntegerType(), True),
-            StructField("payment_type", StringType(), True),
-            StructField("payment_value", FloatType(), True)
-            ]
-        )
+
     # will add locgic to check in postgres if its processed or not
 
     # check file list , if anything exists
@@ -34,12 +19,49 @@ def process_available_files():
     LANDING_ZONE_payments = "/home/DataStuff/data/Olist_e-commerce/Payment_chunks"
     SilverLayerPath = "/home/DataStuff/data/Olist_e-commerce/SilverLayer"
 
+    PaymentFactLocation = SilverLayerPath+"/Payment_fact"
+    OrderSummaryLocation = SilverLayerPath+"/Order_summary"
+
     all_files_orders = glob.glob(f"{LANDING_ZONE_orders}/*.parquet")
     all_files_payments = glob.glob(f"{LANDING_ZONE_payments}/*.parquet")
     if not all_files_orders and  not all_files_payments:
         print(f"Checked {LANDING_ZONE_orders} and {LANDING_ZONE_payments} ")
         print("No new files found to process...")
         return    
+    #---------------------------------------------#
+    # Schema creation
+    payment_fact_schema = StructType(
+    [
+        StructField("order_id", StringType(), False),
+        StructField("payment_sequential", LongType(), True),
+        StructField("payment_installments", IntegerType(), True),
+        StructField("payment_type", StringType(), True),
+        StructField("payment_value", FloatType(), True)
+        ]
+    )
+    order_summary_schema = StructType(
+    [
+        StructField("order_id", StringType(), False),
+        StructField("customer_id", StringType(), True),
+        StructField("order_status", StringType(), True),
+        StructField("order_approved_at", TimestampType(), True),
+        StructField("order_purchase_timestamp", TimestampType(), True),
+        StructField("order_delivered_carrier_date", TimestampType(), True),
+        StructField("order_delivered_customer_date", TimestampType(), True),
+        StructField("order_estimated_delivery_date", TimestampType(), True),
+        StructField("no_of_payments", LongType(), True),   # max(payment_sequential)
+        StructField("payment_installments", IntegerType(), True),   # max(payment_installments)
+        StructField("payment_types_used", StringType(), True),   # listagg(payment_type)
+        StructField("total_payment_value", FloatType(), True)   # sum(payment_value)
+        ]
+    )   
+    timestamp_columns = [
+    "order_approved_at",
+    "order_purchase_timestamp", 
+    "order_delivered_carrier_date",
+    "order_delivered_customer_date",
+    "order_estimated_delivery_date"
+        ]
 # -------------------------------------------#    
     # start spark if any file exists to load
     spark, logger = start_spark()
@@ -48,12 +70,12 @@ def process_available_files():
             print(f"Processing Order files...")
             print(all_files_orders)
             order_df = spark.read.parquet(LANDING_ZONE_orders)
-            for ts_col in ['c']:
+            for ts_col in timestamp_columns:
                 if ts_col in order_df.columns:
                     order_df = order_df.withColumn(ts_col, to_timestamp(col(ts_col)))
         else:
             print("No order files to load...")
-            order_df = spark.createDataFrame([],master_schema )
+            order_df = spark.createDataFrame([],order_summary_schema )
         
         if all_files_payments:
             print(f" Processing Order files...")
@@ -62,91 +84,124 @@ def process_available_files():
             # casting a column as spark is taking it as binary
             if "payment_installments" in payment_df.columns:
                 payment_df = payment_df.withColumn("payment_installments", col("payment_installments").cast(IntegerType()))
+            
         else:
             print("No payment files to load...")
-            payment_df = spark.createDataFrame([],master_schema )
-    #---------------------------------------------#
-        master_df = order_df.alias("o").join(
-            payment_df.alias("p"), 
-            on='order_id',
-            how='outer'
-            ).select(
-                coalesce(col("o.order_id"), col("p.order_id")).alias("order_id"),
-                col("o.customer_id"),
-                col("o.order_status"),
-                col("o.order_approved_at"),
-                col("o.order_purchase_timestamp"),
-                col("o.order_delivered_carrier_date"),
-                col("o.order_delivered_customer_date"),
-                col("o.order_estimated_delivery_date"),
-                col("p.payment_sequential"),
-                col("p.payment_installments"),
-                col("p.payment_type"),
-                col("p.payment_value")
-                    )  
-        
-        #print(f"MasterSchmema I am using : {master_df.printSchema()}")
+            payment_df = spark.createDataFrame([],payment_fact_schema )
+    
+    #-----------------------------------------------#
+        # creating payment fact table
         try:
-            delta_table = DeltaTable.forPath(spark,SilverLayerPath)
-            print("Delta table exists, loading...")
+            payment_fact_table = DeltaTable.forPath(spark,PaymentFactLocation)
+            print("Payment_fact table exists, loading...")
         except AnalysisException:
-            print("Delta table doesn't exist, creating...")
-            spark.createDataFrame([], master_schema) \
+            print("Payment_fact table doesn't exists.... creating")
+            spark.createDataFrame([], payment_fact_schema) \
                 .write \
                 .format("delta") \
                 .mode("overwrite") \
-                .save(SilverLayerPath)
-            delta_table = DeltaTable.forPath(spark, SilverLayerPath)
+                .save(PaymentFactLocation)
+            payment_fact_table = DeltaTable.forPath(spark, PaymentFactLocation)
         except Exception as e:
             print(e)
             return
-        print("Moving data to delta tables..")
-
-        # S for delta table record, C for the current processing file record
-        # if a record exists partially, below will update with relevant values
+        # Order Summary table    
         try:
-            delta_table.alias("S")\
+            order_summary_table = DeltaTable.forPath(spark,OrderSummaryLocation)
+            print("Order_summary table exists, loading...")
+        except AnalysisException:
+            print("Order_summary table doesn't exist, creating...")
+            spark.createDataFrame([], order_summary_schema) \
+                .write \
+                .format("delta") \
+                .mode("overwrite") \
+                .save(OrderSummaryLocation)
+            order_summary_table = DeltaTable.forPath(spark, OrderSummaryLocation)
+        except Exception as e:
+            print(e)
+            return
+    #-----------------------------------------------------#
+    # adding record from new payment files (if any) to payment_fact table
+        try:
+            payment_fact_table.alias("A") \
                 .merge(
-                    master_df.alias("C"),
-                    "S.order_id = C.order_id"
+                    payment_df.alias("B"),
+                    "A.order_id = B.order_id AND A.payment_sequential=B.payment_sequential"
                 ) \
-                .whenMatchedUpdate(
-                    set = {
-                "order_id": "COALESCE(C.order_id,S.customer_id)",
-                "customer_id": "COALESCE(C.customer_id, S.customer_id)",
-                "order_status": "COALESCE(C.order_status, S.order_status)",
-                "order_approved_at": "COALESCE(C.order_approved_at, S.order_approved_at)",
-                "order_purchase_timestamp": "COALESCE(C.order_purchase_timestamp, S.order_purchase_timestamp)",
-                "order_delivered_carrier_date": "COALESCE(C.order_delivered_carrier_date, S.order_delivered_carrier_date)",
-                "order_delivered_customer_date": "COALESCE(C.order_delivered_customer_date, S.order_delivered_customer_date)",
-                "order_estimated_delivery_date": "COALESCE(C.order_estimated_delivery_date, S.order_estimated_delivery_date)",
-                "payment_sequential": "COALESCE(C.payment_sequential, S.payment_sequential)",
-                "payment_installments": "COALESCE(C.payment_installments, S.payment_installments)",
-                "payment_type": "COALESCE(C.payment_type, S.payment_type)",
-                "payment_value": "COALESCE(C.payment_value, S.payment_value)",
-                }
-                    
-                ) \
+                .whenMatchedUpdateAll() \
                 .whenNotMatchedInsertAll() \
                 .execute()
         except Exception as e:
-            print("Error in savings in delta table")
+            print("Error while adding records to payment_fact table...")
             print(e)
-            print("Aborting the process for now...")    
-            return     
-
-        '''
-        master_df.write \
-            .format("delta") \
-            .mode("append") \
-            .option("mergeSchema",True) \
-            .save(SilverLayerPath)
-        '''
-
-
-
-        print("Succesfully moved to delta table")
-
+            return
+        
+    # taking payment summary to be merged in to order table
+        print("Trying to read payment_fact_table")
+        payment_fact_data = spark.read.format("delta").load(PaymentFactLocation)
+        
+        print("Payment_fact_table read successful!!")
+        payment_summary = payment_fact_data.groupBy(col("order_id")).agg(
+            sum("payment_value").alias("total_payment_value"), 
+            max("payment_installments").alias("payment_installments"),
+            max("payment_sequential").alias("no_of_payments"),  
+            concat_ws(",", sort_array(collect_set("payment_type"))).alias("payment_types_used")
+        )
+        print("Successfully aggregated the data from the updated payment fact table....")
+        print("Trying to join the record to order file")
+        
+    # Join payment_summary to Order data from files   
+        current_master_order_summary = order_df.alias("o").join(
+            payment_summary.alias("p"),
+            on='order_id',
+            how='outer'
+        ).select(
+                    coalesce(col("o.order_id"), col("p.order_id")).alias("order_id"),
+                    col("o.customer_id"),
+                    col("o.order_status"),
+                    col("o.order_approved_at"),
+                    col("o.order_purchase_timestamp"),
+                    col("o.order_delivered_carrier_date"),
+                    col("o.order_delivered_customer_date"),
+                    col("o.order_estimated_delivery_date"),
+                    col("p.no_of_payments"),
+                    col("p.payment_installments"),
+                    col("p.payment_types_used"),
+                    col("p.total_payment_value")
+                )
+        print("Updated payment summary has been joined with latest order file...")
+        
+    # merge to order summary fact table
+        print("Merging the orderSummary fact table with the latest summary created...")
+        order_summary_table.alias("o") \
+            .merge(current_master_order_summary.alias("m"), "o.order_id=m.order_id") \
+            .whenMatchedUpdate(set ={
+                "order_id": "m.order_id",
+                "customer_id": "COALESCE(m.customer_id, o.customer_id)",
+                "order_status": "COALESCE(m.order_status, o.order_status)",
+                "order_approved_at": "COALESCE(m.order_approved_at, o.order_approved_at)",
+                "order_purchase_timestamp": "COALESCE(m.order_purchase_timestamp, o.order_purchase_timestamp)",
+                "order_delivered_carrier_date": "COALESCE(m.order_delivered_carrier_date, o.order_delivered_carrier_date)",
+                "order_delivered_customer_date": "COALESCE(m.order_delivered_customer_date, o.order_delivered_customer_date)",
+                "order_estimated_delivery_date": "COALESCE(m.order_estimated_delivery_date, o.order_estimated_delivery_date)",
+                "no_of_payments": "m.no_of_payments",
+                "payment_installments": "m.payment_installments",
+                "payment_types_used": "m.payment_types_used",
+                "total_payment_value": "m.total_payment_value"
+            }) \
+            .whenNotMatchedInsertAll() \
+            .execute()
+        print("merge complete...")
+        
+        """
+        Current Payment File → Merge into Payment Facts (append new payments)
+                                    ↓
+                            Read ENTIRE Payment Facts table
+                                    ↓
+                                Aggregate ALL payments per order
+                                    ↓
+                            Update Order Summary with COMPLETE totals
+        """
     #----------------------------------------------#
         # 3. After successful merge, move the file to a 'processed' folder
         archive_order_dir = f"{LANDING_ZONE_orders}/processed/"
@@ -164,6 +219,7 @@ def process_available_files():
 
     except Exception as e:
         print("Something wrong happened!!!!")
+        print(e)
     finally:
         spark.stop()
 
